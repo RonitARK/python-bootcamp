94b92141fe57391653d374741840c57b2deb74ba
Ronit Gupta <139333896+RonitARK@users.noreply.github.com>
2026-07-01 23:12:35 +0530

    Delete Age Calculator.py

--- Age Calculator.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-today = datetime.now()
-dob = input("Enter your date of birth (DD,MM,YYYY): ").split(",")
-age = today.year - int(dob[2])
-print(f"You are {age} years old")
